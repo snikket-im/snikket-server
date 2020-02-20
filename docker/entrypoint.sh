c30b7310aaf5f5a2742c596e52da9019b78f200b
Matthew Wild <mwild1@gmail.com>
2020-02-20 22:00:14 +0000

    Add support for PUID/PGID and create necessary directories in /snikket
    This is useful when e.g. mounting an existing directory instead of a
    docker-managed volume.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -15,4 +15,28 @@ echo "from snikket@$SNIKKET_DOMAIN" >> /etc/msmtprc
 
 unset SNIKKET_SMTP_URL
 
+PUID=${PUID:=$(stat -c %u /snikket)}
+PGID=${PGID:=$(stat -c %g /snikket)}
+
+if [ "$PUID" != 0 ] && [ "$PGID" != 0 ]; then
+	usermod  -o -u "$PUID" prosody
+	groupmod -o -g "$PGID" prosody
+
+	usermod  -o -u "$PUID" letsencrypt
+	groupmod -o -g "$PGID" letsencrypt
+fi
+
+if ! test -d /snikket/prosody; then
+	install -o prosody -g prosody -m 750 -d /snikket/prosody;
+fi
+
+chown -R prosody:prosody /var/spool/anacron /var/run/prosody /snikket/prosody /etc/prosody
+
+if ! test -d /snikket/letsencrypt; then
+	install -o letsencrypt -g letsencrypt -m 750 -d /snikket/letsencrypt;
+fi
+
+install -o letsencrypt -g letsencrypt -m 750 -d /var/lib/letsencrypt;
+install -o letsencrypt -g letsencrypt -m 750 -d /var/log/letsencrypt;
+
 exec supervisord -c /etc/supervisor/supervisord.conf
