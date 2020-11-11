668592c87a5577cf467bcb4ab027f4b2ccbcba96
Matthew Wild <mwild1@gmail.com>
2020-11-11 16:42:08 +0000

    mod_http_libjs: Allow configurable path

--- snikket-modules/mod_http_libjs/mod_http_libjs.lua
+++ snikket-modules/mod_http_libjs/mod_http_libjs.lua
@@ -3,9 +3,11 @@ local mime_map = module:shared("/*/http_files/mime").types or {
 	js = "application/javascript",
 };
 
+local libjs_path = module:get_option_string("libjs_path", "/usr/share/javascript");
+
 module:provides("http", {
 		default_path = "/share";
 		route = {
-			["GET /*"] = require "net.http.files".serve({ path = "/usr/share/javascript", mime_map = mime_map });
+			["GET /*"] = require "net.http.files".serve({ path = libjs_path, mime_map = mime_map });
 		}
 	});
