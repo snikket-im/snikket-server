6cb87092bf12780f319ece9dd6c5474688927050
Matthew Wild <mwild1@gmail.com>
2021-01-27 16:23:12 +0000

    mod_authz_internal: Remove in favour of upstream (prosody) module

--- snikket-modules/mod_authz_internal/mod_authz_internal.lua
+++
@@ -1,9 +0,0 @@
-local role_store = module:open_store("roles");
-
-function get_user_roles(user)
-	return role_store:get(user);
-end
-
-function get_jid_roles(jid) --luacheck: ignore 212/jid
-	return nil;
-end
