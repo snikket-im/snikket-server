c4a8a88028a9e1fa185cd226094b126d99eb40f6
Matthew Wild <mwild1@gmail.com>
2021-03-09 15:28:09 +0000

    docker: Remove obsolete letsencrypt things (moved to cert-manager)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -30,9 +30,6 @@ PGID=${PGID:=$(stat -c %g /snikket)}
 if [ "$PUID" != 0 ] && [ "$PGID" != 0 ]; then
 	usermod  -o -u "$PUID" prosody
 	groupmod -o -g "$PGID" prosody
-
-	usermod  -o -u "$PUID" letsencrypt
-	groupmod -o -g "$PGID" letsencrypt
 fi
 
 if ! test -d /snikket/prosody; then
@@ -40,16 +37,6 @@ if ! test -d /snikket/prosody; then
 fi
 
 chown -R prosody:prosody /var/spool/anacron /var/run/prosody /snikket/prosody /etc/prosody
-
-if ! test -d /snikket/letsencrypt; then
-	install -o letsencrypt -g letsencrypt -m 750 -d /snikket/letsencrypt;
-fi
-
-install -o letsencrypt -g letsencrypt -m 750 -d /var/lib/letsencrypt;
-install -o letsencrypt -g letsencrypt -m 750 -d /var/log/letsencrypt;
-install -o letsencrypt -g letsencrypt -m 755 -d /var/www/.well-known/acme-challenge;
-
-chown -R letsencrypt:letsencrypt /snikket/letsencrypt
 
 ## Generate secret for coturn auth if necessary
 if ! test -f /snikket/prosody/turn-auth-secret-v2; then
